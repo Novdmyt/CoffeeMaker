f9d831d034861bd8ed8c8799125b0e4d84384517
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-03 11:58:41 +0300

    Creating a method startDrink()

--- src/app/drinks/Drinks.java
+++ src/app/drinks/Drinks.java
@@ -6,6 +6,8 @@ public class Drinks {
     public void setDrink(Drink drink){
         this.drink = drink;
     }
-
+    public void startDrink(){
+        System.out.printf("Your %s. Enjoy :)", drink.prepare());
+    }
 
 }
