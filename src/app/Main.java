adc253bf93de7eff1f6601118f6392e12f4d830c
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-03 12:02:45 +0300

    Create main method

---
+++ src/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+import app.CoffeeMachine.CoffeeMachine;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+        coffeeMachine.getMenu();
+    }
+}
